d50cb332b546d89a4ff3117e6eac04a77eea76c0
Alecs <erick.borges@outlook.es>
2024-02-21 19:38:35 -0600

    feature: ProfileScreen UserProfile view created

---
+++ components/CardOptions.js
@@ -0,0 +1,51 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+import { Entypo } from '@expo/vector-icons';
+import Styles from './Styles';
+import React from 'react'
+
+const CardOptions = ({ optionName, onPress, signOut }) => {
+    return (
+        <View>
+            {signOut ?
+                <Pressable style={styles.flatCard} onPress={onPress}>
+                    <View style={{ width: '100%' }}>
+                        <Text style={Styles.textOut}> {optionName} </Text>
+                    </View>
+                </Pressable>
+
+                :
+                <View style={styles.flatCard}>
+                    <View style={{ width: '90%' }}>
+                        <Text style={Styles.text}> {optionName} </Text>
+                    </View>
+                    <View style={{ width: '20%', alignItems: 'flex-end' }}>
+                        <Entypo name="chevron-right" size={24} color="#fff" onPress={onPress} />
+                    </View>
+                </View>
+            }
+        </View>
+        
+    )
+}
+
+export default CardOptions
+
+const styles = StyleSheet.create({
+
+    flatCard: {
+        flexDirection: 'row',
+        borderTopWidth: 2,
+        borderBottomWidth: 2,
+        borderTopColor: '#2C2C2E',
+        borderBottomColor: '#2C2C2E',
+        width: '85%',
+        padding: 20,
+    },
+
+    flatText: {
+        fontSize: 18,
+        fontWeight: 500,
+        color: '#fff',
+    },
+
+})
--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,10 +1,51 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, Platform } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+//Styles
+import Styles from '../components/Styles';
+//Components
+import CardOptions from '../components/CardOptions';
 
 const ProfileScreen = () => {
+  const navigation = useNavigation();
+  const userPremium = false;
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={Styles.container}>
+      <View>
+        {/* ProfileView */}
+        <View style={Styles.profileView}>
+          {/* ImageProfile */}
+          <View style={userPremium ? Styles.imageProfileViewPremium : Styles.imageProfileView}>
+            <Image style={Styles.profileImage} source={require('../images/trainer.jpg')} />
+          </View>
+          {/* divisionLine */}
+          <View style={{ borderLeftWidth: 2, borderLeftColor: '#2C2C2E', }} />
+          {/* joinedTime */}
+          <View style={{ justifyContent: 'center', marginRight: 20 }}>
+            <View>
+              <Text style={Styles.joinedTxt}>Joined</Text>
+              <Text style={Styles.text}>2 days ago</Text>
+            </View>
+          </View>
+        </View>
+        {/* profileName */}
+        <View style={{ marginBottom: '10%' }}>
+          <View style={{ width: '80%', marginTop: 20, marginLeft: 20 }}>
+            <Text style={Styles.header}>SARAH WEGAN</Text>
+          </View>
+        </View>
+        {/* profileOptions */}
+        <View style={{ alignItems: 'center' }}>
+          <CardOptions optionName={'Edit Profile'} onPress={() => console.log('Edit Profile')} />
+          <CardOptions optionName={'Privacy Policy'} onPress={() => console.log('Privacy Policy')} />
+          <CardOptions optionName={'Settings'} onPress={() => console.log('Settings')} />
+        </View>
+        {/* signOut */}
+        <View style={{ alignItems: 'center', marginTop: Platform.OS == 'ios' ? '20%' : '12%' }}>
+          <CardOptions optionName={'Sign Out'} signOut={true} onPress={() => console.log('Sign Out')} />
+        </View>
+      </View>
     </View>
   )
 }
