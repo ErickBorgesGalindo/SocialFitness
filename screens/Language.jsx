753be2d8cbc79fdf4c7da20f87a2e5ab0eaaf0eb
Alecs <erick.borges@outlook.es>
2024-02-27 12:47:05 -0600

    bugfix: LanguageView flatlist

--- screens/Language.jsx
+++ screens/Language.jsx
@@ -1,11 +1,24 @@
-import { View, Text, ScrollView, Platform } from 'react-native';
+import { View, Text, Platform, FlatList, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 //Styles
 import Styles from '../components/Styles';
 //Components
 import BackBtn from '../components/BackBtn';
 import FlatCard from '../components/CardFlat';
+
+const data = [
+  { id: 1, name: 'English', image: require('../images/eeuu.png') },
+  { id: 2, name: 'Spanish', image: require('../images/spanish.png') },
+  { id: 3, name: 'Japanesse', image: require('../images/japan.jpg') },
+  { id: 4, name: 'English', image: require('../images/eeuu.png') },
+  { id: 5, name: 'Spanish', image: require('../images/spanish.png') },
+  { id: 6, name: 'Japanesse', image: require('../images/japan.jpg') },
+  { id: 7, name: 'English', image: require('../images/eeuu.png') },
+  { id: 8, name: 'Spanish', image: require('../images/spanish.png') },
+  { id: 9, name: 'Japanesse', image: require('../images/japan.jpg') },
+];
 
 const Language = () => {
 
@@ -29,79 +42,21 @@ const Language = () => {
       <BackBtn style={{ top: '05%' }} onPress={() => navigation.goBack()} />
 
       {/* options */}
-      <ScrollView style={{ marginBottom: Platform.OS === 'ios' ? '10%' : '03%', marginTop: 10 }}>
-        {/* Excercise Cards */}
-        <FlatCard
-          excerciseName='English'
-          // duration='0:30'
-          ImageSource={require('../images/eeuu.png')}
-          onPress={() => console.log('Hugo')} />
-
-        <FlatCard
-          excerciseName='Español'
-          // duration='0:30'
-          ImageSource={require('../images/spanish.png')}
-          onPress={() => console.log('Hugo')} />
-
-        <FlatCard
-          excerciseName='Japan'
-          // duration='0:30'
-          ImageSource={require('../images/japan.jpg')}
-          onPress={() => console.log('Hugo')} />
-
-        <FlatCard
-          excerciseName='English'
-          // duration='0:30'
-          ImageSource={require('../images/eeuu.png')}
-          onPress={() => console.log('Hugo')} />
-
-        <FlatCard
-          excerciseName='Español'
-          // duration='0:30'
-          ImageSource={require('../images/spanish.png')}
-          onPress={() => console.log('Hugo')} />
-
-        <FlatCard
-          excerciseName='Japan'
-          // duration='0:30'
-          ImageSource={require('../images/japan.jpg')}
-          onPress={() => console.log('Hugo')} />
-        <FlatCard
-          excerciseName='English'
-          // duration='0:30'
-          ImageSource={require('../images/eeuu.png')}
-          onPress={() => console.log('Hugo')} />
-
-        <FlatCard
-          excerciseName='Español'
-          // duration='0:30'
-          ImageSource={require('../images/spanish.png')}
-          onPress={() => console.log('Hugo')} />
-
-        <FlatCard
-          excerciseName='Japan'
-          // duration='0:30'
-          ImageSource={require('../images/japan.jpg')}
-          onPress={() => console.log('Hugo')} />
-        <FlatCard
-          excerciseName='English'
-          // duration='0:30'
-          ImageSource={require('../images/eeuu.png')}
-          onPress={() => console.log('Hugo')} />
-
-        <FlatCard
-          excerciseName='Español'
-          // duration='0:30'
-          ImageSource={require('../images/spanish.png')}
-          onPress={() => console.log('Hugo')} />
-
-        <FlatCard
-          excerciseName='Japan'
-          // duration='0:30'
-          ImageSource={require('../images/japan.jpg')}
-          onPress={() => console.log('Hugo')} />
-
-      </ScrollView>
+      <View style={{ alignItems: 'center' }}>
+        <FlatList
+          style={{ width: Platform.OS === 'ios' ? '100%' : '110%', marginBottom:'30%'}}
+          data={data}
+          renderItem={({ item }) => {
+            return (
+              <GestureHandlerRootView style={{ marginLeft: 15 }}>
+                <TouchableOpacity>
+                  <FlatCard data={item} onPress={() => handleCardPress(item)} />
+                </TouchableOpacity>
+              </GestureHandlerRootView>
+            )
+          }}
+        />
+      </View>
     </View>
   )
 }
