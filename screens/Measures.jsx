9935175aff2a81a7ec29208c0b302344870bd78c
Alecs <erick.borges@outlook.es>
2024-03-01 21:47:17 -0600

    Order corrections, nothing important

--- screens/Measures.jsx
+++ screens/Measures.jsx
@@ -16,21 +16,8 @@ const Measures = () => {
   const [isDayPressed, setIsDayPressed] = useState(false);
   const [selectedDay, setSelectedDay] = useState(null);
   const [month, setMonth] = useState(moment().month()); 
-  const [currentYear, setCurrentYear] = useState(moment().year()); 
-
-  const handleDayPress = (index) => {
-    setIsDayPressed(index); // Actualiza solo el índice del día presionado
-    setSelectedDay(getDates(month)[index]);
-    // console.log( getDayName(selectedDay.day),':',selectedDay.number);
-    // console.log(Día seleccionado: ${selectedDay.number} - ${getDayName(selectedDay.day)}'');
-  };
-
-  useEffect(() => {
-    const todayIndex = getDates(month).findIndex((day) => day.number === new Date().getDate());
-    setIsDayPressed(todayIndex);
-  }, []);
-
-  // Juntar toda la data en 1 solo o 2 const
+  const [currentYear, setCurrentYear] = useState(moment().year());
+  
   const dataCal = {
     labels: ["Calories"],
     data: [0.8],
@@ -62,6 +49,18 @@ const Measures = () => {
     { id: 4, name: 'Stability Training', time: '10:00' },
     { id: 5, name: 'End', time: '20:00' },
   ]
+
+  const handleDayPress = (index) => {
+    setIsDayPressed(index); // Actualiza solo el índice del día presionado
+    setSelectedDay(getDates(month)[index]);
+    // console.log( getDayName(selectedDay.day),':',selectedDay.number);
+    // console.log(Día seleccionado: ${selectedDay.number} - ${getDayName(selectedDay.day)}'');
+  };
+
+  useEffect(() => {
+    const todayIndex = getDates(month).findIndex((day) => day.number === new Date().getDate());
+    setIsDayPressed(todayIndex);
+  }, []);
 
   function getDates(month) {
     const year = 2024; // Cambiar el año según sea necesario
@@ -129,12 +128,6 @@ const Measures = () => {
             />
           ))}
         </ScrollView>
-
-        {/* {selectedDay && ( // Muestra el texto solo si hay un día seleccionado
-        <Text style={{ ...Styles.text, fontSize: 16, marginTop: 10 }}>
-          Día seleccionado: {selectedDay.number} - {getDayName(selectedDay.day)}
-        </Text>
-      )} */}
 
       </View>
       {/* Calories */}
@@ -237,109 +230,3 @@ const Measures = () => {
 }
 
 export default Measures
-
-
-// import { View, Dimensions, Platform} from 'react-native';
-// import React, { useState, useEffect } from 'react';
-// // Styles
-// import Styles from '../components/Styles';
-// // Components
-// import Measure from '../components/Measure';
-
-// const Measures = () => {
-//   const screenWidth = Dimensions.get('window').width;
-//   const screenHeight = Dimensions.get('window').height;
-
-//   const dataCal = {
-//     labels: ["Calories"],
-//     data: [0.8],
-//     amount: 652
-//   };
-
-//   const data = [
-//     { id: 1, name: 'Stability Training', time: '10:00', labels:["Steps"], data:[0.6], amount:[6540] },
-//     { id: 2, name: 'Flash Cycling', time: '20:00', labels:["Time"], data:[0.4], amount:[45] },
-//     { id: 3, name: 'Running', time: '20:00', labels:["Heart"], data:[0.7], amount:[72] },
-//     { id: 4, name: 'Stability Training', time: '10:00' },
-//     { id: 5, name: 'End', time: '20:00' },
-//   ]
-//   return (
-//     <View style={Styles.container}>
-//       {/* Calories */}
-//       <View style={{height:'20%'}}>
-//         <Measure
-//           data={dataCal}
-//           screenWidth={screenWidth}
-//           screenHeight={screenHeight / 3}
-//           strokeWidth={5}
-//           radius={90}
-//           section={1}
-//           strokeColor='255, 255, 255'
-//           top={Platform.OS === 'ios' ? '115%' : '90%'}
-//           text={'Active Calories'}
-//           amount={`${dataCal.amount} Cal`}
-//           textPosition={Platform.OS === 'ios' ? '110%' : '80%'}
-//           subTextPosition={Platform.OS === 'ios' ? '60%' : '20%'}
-//         />
-//       </View>
-//       {/* AnotherMeasures */}
-//       <View style={{ flexDirection: 'row', marginTop: Platform.OS == 'ios' ? '25%' : '32%', height: '10%' }}>
-//         {/* Steps */}
-//         <View>
-//           <Measure
-//             data={dataSteps}
-//             screenWidth={screenWidth / 3}
-//             screenHeight={screenHeight / 3}
-//             strokeWidth={5}
-//             radius={55}
-//             section={3}
-//             strokeColor='208, 253, 62'
-//             top={Platform.OS === 'ios' ? '20%' : '-10%'}
-//             textPosition={Platform.OS === 'ios' ? '-20%' : '-50%'}
-//             subTextPosition={Platform.OS === 'ios' ? '-20%' : '-55%'}
-//             text='Steps'
-//             amount={dataSteps.amount}
-//           />
-//         </View>
-
-//         {/* Time */}
-//         <View>
-//           <Measure
-//             data={dataTime}
-//             screenWidth={screenWidth / 3}
-//             screenHeight={screenHeight / 3}
-//             strokeWidth={5}
-//             radius={55}
-//             section={3}
-//             strokeColor='255, 36, 36'
-//             top={Platform.OS === 'ios' ? '20%' : '-10%'}
-//             textPosition={Platform.OS === 'ios' ? '-20%' : '-50%'}
-//             subTextPosition={Platform.OS === 'ios' ? '-20%' : '-50%'}
-//             text='Minutes'
-//             amount={dataTime.amount}
-//           />
-//         </View>
-
-//         {/* HeartRate */}
-//         <View>
-//           <Measure
-//             data={dataHeart}
-//             screenWidth={screenWidth / 3}
-//             screenHeight={screenHeight / 3}
-//             strokeWidth={5}
-//             radius={55}
-//             section={3}
-//             strokeColor='231, 147, 50'
-//             top={Platform.OS === 'ios' ? '20%' : '-10%'}
-//             textPosition={Platform.OS === 'ios' ? '-20%' : '-50%'}
-//             subTextPosition={Platform.OS === 'ios' ? '-20%' : '-50%'}
-//             text='BPM'
-//             amount={dataHeart.amount}
-//           />
-//         </View>
-//       </View>
-//     </View>
-//   )
-// }
-
-// export default Measures
